0f26208840a7c62ff6aa64373ad9b31018dc2e3f
nesumi <nesumidesu@gmail.com>
2023-07-17 10:33:19 -0400

    arreglosdecodigo

--- src/componentes/Card.jsx
+++ src/componentes/Card.jsx
@@ -1,6 +1,19 @@
 import { useState, useEffect } from 'react';
 import '../css/estilos.css';
 
+const ListaPokemon = [
+  'https://pokeapi.co/api/v2/pokemon/1',
+  'https://pokeapi.co/api/v2/pokemon/4',
+  'https://pokeapi.co/api/v2/pokemon/7',
+  'https://pokeapi.co/api/v2/pokemon/8',
+  'https://pokeapi.co/api/v2/pokemon/9',
+  'https://pokeapi.co/api/v2/pokemon/10',
+  'https://pokeapi.co/api/v2/pokemon/11',
+  'https://pokeapi.co/api/v2/pokemon/12',
+  'https://pokeapi.co/api/v2/pokemon/13',
+  'https://pokeapi.co/api/v2/pokemon/14'
+];
+
 function Card(props) {
   const [pokemon, setPokemon] = useState({});
   const [isLoading, setIsLoading] = useState(true);
@@ -8,12 +21,9 @@ function Card(props) {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await fetch('https://pokeapi.co/api/v2/pokemon');
+        const response = await fetch(props.link);
         const data = await response.json();
-        const randomPokemonIndex = Math.floor(Math.random() * data.count) + 1;
-        const pokemonResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${randomPokemonIndex}`);
-        const pokemonData = await pokemonResponse.json();
-        setPokemon(pokemonData);
+        setPokemon(data);
         setIsLoading(false);
       } catch (error) {
         console.error(error);
@@ -21,27 +31,29 @@ function Card(props) {
     };
 
     fetchData();
-  }, []);
+  }, [props.link]);
 
   if (isLoading) {
     return <div>Cargando.....</div>;
   }
 
   return (
-    <div className='contenedorTexto'> 
+    <div className='contenedorTexto'>
       <img className='contenedorImagen' src={pokemon.sprites.front_default} alt='imagen' />
       <h5 className='contenedorTitulo'>{pokemon.name}</h5>
-      <p className='contenedorParrafo'>Especie:"Pokemon"</p>
+      <p className='contenedorParrafo'>Especie: "Pokemon"</p>
     </div>
   );
 }
 
-function CardContainer() {
-  const cards = [];
-  for (let i = 0; i < 10; i++) {
-    cards.push(<Card key={i} />);
-  }
-  return <div className="card-container">{cards}</div>;
+function Cartas() {
+  return (
+    <div className="Cartass">
+      {ListaPokemon.map((link, index) => (
+        <Card key={index} link={link} />
+      ))}
+    </div>
+  );
 }
 
-export default CardContainer;
+export default Cartas;
